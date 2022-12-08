100c7816b779d5092a42fdf8a2fe2caaf748b6f6
dmitchley <69771055+dmitchley@users.noreply.github.com>
2022-12-09 00:27:03 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -6,20 +6,7 @@ import Script from "next/script";
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Script
-        strategy="lazyOnload"
-        src={`https://www.googletagmanager.com/gtag/js?id=G-ZQ48DQ5YP2`}
-      />
-
-      <Script strategy="lazyOnload">
-        {`
-             window.dataLayer = window.dataLayer || [];
-             function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-           
-             gtag('config', 'G-ZQ48DQ5YP2');
-                `}
-      </Script>
+      
 
       <Header />
 
